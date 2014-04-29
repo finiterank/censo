cab98cddf22c345f6317798fd532b94b7135dfe0
Javier Moreno <bluelephant@gmail.com>
2014-04-28 20:40:12 -0400

    Diagrama y dual

--- js/desplazados.js
+++ js/desplazados.js
@@ -1,115 +1,412 @@
+/*** Define parameters and tools ***/
 var width = 750,
     height = 750,
-    outerRadius = Math.min(width, height) / 2 - 10,
-    innerRadius = outerRadius - 24;
+    outerRadius = Math.min(width, height) / 2 - 100,
+    innerRadius = outerRadius - 18;
 
-var formatPercent = d3.format(".2%");
-
-var last_layout = {};
+//string url for the initial data set
+//would usually be a file path url, here it is the id
+//selector for the <pre> element storing the data
 
 var color = d3.scale.category20b();
 
+
+//create number formatting functions
+var formatPercent = d3.format("%");
+
+function numberWithCommas(x){
+    return x.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+//create the arc path data generator for the groups
 var arc = d3.svg.arc()
     .innerRadius(innerRadius)
     .outerRadius(outerRadius);
 
-var layout = d3.layout.chord()
-    .padding(.04)
-    .sortSubgroups(d3.descending)
-    .sortChords(d3.ascending);
-
+//create the chord path data generator for the chords
 var path = d3.svg.chord()
     .radius(innerRadius);
 
-var svg = d3.select("#diagrama").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("id", "circle")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+//define the default chord layout parameters
+//within a function that returns a new layout object;
+//that way, you can create multiple chord layouts
+//that are the same except for the data.
+function getDefaultLayout() {
+    return d3.layout.chord()
+    .padding(0.03)
+    .sortSubgroups(d3.descending)
+    .sortChords(d3.ascending);
+}
+var last_layout; //store layout between updates
+var neighborhoods; //store neighbourhood data outside data-reading function
 
+/*** Initialize the visualization ***/
+var g = d3.select("#diagrama").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+    .append("g")
+        .attr("id", "circle")
+        .attr("transform",
+              "translate(" + width / 2 + "," + height / 2 + ")");
+//the entire graphic will be drawn within this <g> element,
+//so all coordinates will be relative to the center of the circle
 
-
-svg.append("circle")
+g.append("circle")
     .attr("r", outerRadius);
+//this circle is set in CSS to be transparent but to respond to mouse events
+//It will ensure that the <g> responds to all mouse events within
+//the area, even after chords are faded out.
+
+/*** Read in the neighbourhoods data and update with initial data matrix ***/
+//normally this would be done with file-reading functions
+//d3.csv and d3.json and callbacks,
+//instead we're using the string-parsing functions
+//d3.csv.parse and JSON.parse, both of which return the data,
+//no callbacks required.
 
 queue()
     .defer(d3.text, "datos/matriz-desplazamientos.csv")
     .defer(d3.text, "datos/dept-despl.csv")
     .await(ready);
 
+var m, transpuesta, sumasdesplazados, departamentos
+
 function ready(error, matriz, dept){
 
-    var departamentos = d3.csv.parseRows(dept)[0];
+    departamentos = d3.csv.parseRows(dept)[0];
 
-    console.log(departamentos);
-
-    var m = d3.csv.parseRows(matriz).map(function(row){
+    m = d3.csv.parseRows(matriz).map(function(row){
         return row.map(function(value) {
             return +value;
         });
     });
 
-    var transpuesta =  d3.transpose(m);
+    transpuesta =  d3.transpose(m);
 
-    var sumasdesplazados = transpuesta.map(function(d){
+    sumasdesplazados = transpuesta.map(function(d){
         return d.reduce(function(a,b){
             return a+b;
         });
     });
 
-    console.log(sumasdesplazados);
+    updateChords(m, 0);
+}
 
-    layout.matrix(m);
 
-    var group = svg.selectAll(".group")
-        .data(layout.groups)
-        .enter().append("g")
-        .attr("class", "group")
-        .on("mouseover", mouseover);
+/* Create OR update a chord layout from a data matrix */
+function updateChords(matrix, modo) {
 
-    group.append("title").text(function(d, i) {
-      var output = "Desplazados provenientes de " + departamentos[i] + ": " + d.value.toFixed(0);
-      output += "\n" + "Desplazados asentados en " + departamentos[i] + ": " + sumasdesplazados[i];
-      return output;
-    });
+    /* Compute chord layout. */
+    layout = getDefaultLayout(); //create a new layout object
+    layout.matrix(matrix);
 
-    var groupPath = group.append("path")
-        .attr("id", function(d, i) { return "group" + i; })
-        .attr("d", arc)
-        .style("fill", function(d, i) { return color(departamentos[i]); });
+    /* Create/update "group" elements */
+    var groupG = g.selectAll("g.group")
+        .data(layout.groups(), function (d) {
+            return d.index;
+            //use a key function in case the
+            //groups are sorted differently between updates
+        });
 
-    var chord = svg.selectAll(".chord")
-        .data(layout.chords)
-      .enter().append("path")
-        .attr("class", "chord")
-        .style("fill", function(d) { return color(departamentos[d.source.index]); })
-        .attr("d", path);
+    groupG.exit()
+        .transition()
+            .duration(1500)
+            .attr("opacity", 0)
+            .remove(); //remove after transitions are complete
 
-    var groupText = group.append("text")
-        .attr("x", 6)
-        .attr("dy", 15);
+    var newGroups = groupG.enter().append("g")
+        .attr("class", "group");
+    //the enter selection is stored in a variable so we can
+    //enter the <path>, <text>, and <title> elements as well
 
-    groupText.append("textPath")
-        .attr("xlink:href", function(d, i) { return "#group" + i; })
-        .text(function(d, i) { return departamentos[i]; });
 
-    groupText.filter(function(d, i) { return groupPath[0][i].getTotalLength() / 2 - 26 < this.getComputedTextLength(); })
+    //Create the title tooltip for the new groups
+    newGroups.append("title");
+
+    //Update the (tooltip) title text based on the data
+    groupG.select("title")
+        .text(function(d, i) {
+            if (modo === 0){
+                return numberWithCommas(d.value)
+                    + " desplazados dejaron su casa en "
+                    + departamentos[i];
+            }
+            else {
+                return numberWithCommas(d.value)
+                    + " desplazados se asentaron en "
+                    + departamentos[i];
+            }
+        });
+
+    //create the arc paths and set the constant attributes
+    //(those based on the group index, not on the value)
+    newGroups.append("path")
+        .attr("id", function (d) {
+            return "group" + d.index;
+            //using d.index and not i to maintain consistency
+            //even if groups are sorted
+        })
+        .style("fill", function (d) {
+            return color(departamentos[d.index]);
+        });
+
+    //update the paths to match the layout
+    groupG.select("path")
+        .transition()
+            .duration(1500)
+            .attr("opacity", 0.5) //optional, just to observe the transition
+        .attrTween("d", arcTween( last_layout ))
+            .transition().duration(100).attr("opacity", 1) //reset opacity
+        ;
+
+    //create the group labels
+    newGroups.append("svg:text")
+        .attr("xlink:href", function (d) {
+            return "#group" + d.index;
+        })
+        .attr("dy", ".35em")
+        .attr("color", "#fff")
+        .text(function (d) {
+            return departamentos[d.index];
+        });
+
+    //position group labels to match layout
+    groupG.select("text")
+        .transition()
+            .duration(1500)
+            .attr("transform", function(d) {
+                d.angle = (d.startAngle + d.endAngle) / 2;
+                //store the midpoint angle in the data object
+
+                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")" +
+                    " translate(" + (innerRadius + 26) + ")" +
+                    (d.angle > Math.PI ? " rotate(180)" : " rotate(0)");
+                //include the rotate zero so that transforms can be interpolated
+            })
+            .attr("text-anchor", function (d) {
+                return d.angle > Math.PI ? "end" : "begin";
+            });
+
+
+    /* Create/update the chord paths */
+    var chordPaths = g.selectAll("path.chord")
+        .data(layout.chords(), chordKey );
+            //specify a key function to match chords
+            //between updates
+
+
+    //create the new chord paths
+    var newChords = chordPaths.enter()
+        .append("path")
+        .attr("class", "chord");
+
+    // Add title tooltip for each new chord.
+    newChords.append("title");
+
+    // Update all chord title texts
+    chordPaths.select("title")
+        .text(function(d) {
+            if (departamentos[d.target.index] !== departamentos[d.source.index]) {
+                if (modo === 0){
+                    return [numberWithCommas(d.source.value),
+                            " desplazados de ",
+                            departamentos[d.source.index],
+                            " a ",
+                            departamentos[d.target.index],
+                            "\n",
+                            numberWithCommas(d.target.value),
+                            " desplazados de ",
+                            departamentos[d.target.index],
+                            " a ",
+                            departamentos[d.source.index]
+                            ].join("");
+                            //joining an array of many strings is faster than
+                            //repeated calls to the '+' operator,
+                            //and makes for neater code!
+                }
+                else{
+                    return [numberWithCommas(d.source.value),
+                            " desplazados de ",
+                            departamentos[d.target.index],
+                            " a ",
+                            departamentos[d.source.index],
+                            "\n",
+                            numberWithCommas(d.target.value),
+                            " desplazados de ",
+                            departamentos[d.source.index],
+                            " a ",
+                            departamentos[d.target.index]
+                            ].join("");
+                }
+            }
+            else { //source and target are the same
+                return numberWithCommas(d.source.value)
+                    + " desplazados dentro de "
+                    + departamentos[d.source.index];
+            }
+        });
+
+    //handle exiting paths:
+    chordPaths.exit().transition()
+        .duration(1500)
+        .attr("opacity", 0)
         .remove();
 
-    chord.append("title").text(function(d) {
-      return departamentos[d.source.index]
-          + " → " + departamentos[d.target.index]
-          + ": " + d.source.value
-          + "\n" + departamentos[d.target.index]
-          + " → " + departamentos[d.source.index]
-          + ": " + d.target.value;
-    });
+    //update the path shape
+    chordPaths.transition()
+        .duration(1500)
+        .attr("opacity", 0.5) //optional, just to observe the transition
+        .style("fill", function (d) {
+            if (modo === 0){
+                return color(departamentos[d.source.index]);
+            }
+            else {
+                return color(departamentos[d.target.index]);
+            }
+        })
+        .attrTween("d", chordTween(last_layout))
+        .transition().duration(100).attr("opacity", 1) //reset opacity
+    ;
 
-    function mouseover(d, i) {
-      chord.classed("fade", function(p) {
-        return p.source.index != i
-            && p.target.index != i;
-      });
+    //add the mouseover/fade out behaviour to the groups
+    //this is reset on every update, so it will use the latest
+    //chordPaths selection
+    groupG.on("mouseover", function(d) {
+        chordPaths.classed("fade", function (p) {
+            //returns true if *neither* the source or target of the chord
+            //matches the group that has been moused-over
+            return ((p.source.index != d.index) && (p.target.index != d.index));
+        });
+    });
+    //the "unfade" is handled with CSS :hover class on g#circle
+    //you could also do it using a mouseout event:
+    /*
+    g.on("mouseout", function() {
+        if (this == g.node() )
+            //only respond to mouseout of the entire circle
+            //not mouseout events for sub-components
+            chordPaths.classed("fade", false);
+    });
+    */
+
+    last_layout = layout; //save for next update
+
+//  }); //end of d3.json
+}
+
+function arcTween(oldLayout) {
+    //this function will be called once per update cycle
+
+    //Create a key:value version of the old layout's groups array
+    //so we can easily find the matching group
+    //even if the group index values don't match the array index
+    //(because of sorting)
+    var oldGroups = {};
+    if (oldLayout) {
+        oldLayout.groups().forEach( function(groupData) {
+            oldGroups[ groupData.index ] = groupData;
+        });
     }
+
+    return function (d, i) {
+        var tween;
+        var old = oldGroups[d.index];
+        if (old) { //there's a matching old group
+            tween = d3.interpolate(old, d);
+        }
+        else {
+            //create a zero-width arc object
+            var emptyArc = {startAngle:d.startAngle,
+                            endAngle:d.startAngle};
+            tween = d3.interpolate(emptyArc, d);
+        }
+
+        return function (t) {
+            return arc( tween(t) );
+        };
+    };
+}
+
+function chordKey(data) {
+    return (data.source.index < data.target.index) ?
+        data.source.index  + "-" + data.target.index:
+        data.target.index  + "-" + data.source.index;
+
+    //create a key that will represent the relationship
+    //between these two groups *regardless*
+    //of which group is called 'source' and which 'target'
+}
+function chordTween(oldLayout) {
+    //this function will be called once per update cycle
+
+    //Create a key:value version of the old layout's chords array
+    //so we can easily find the matching chord
+    //(which may not have a matching index)
+
+    var oldChords = {};
+
+    if (oldLayout) {
+        oldLayout.chords().forEach( function(chordData) {
+            oldChords[ chordKey(chordData) ] = chordData;
+        });
+    }
+
+    return function (d, i) {
+        //this function will be called for each active chord
+
+        var tween;
+        var old = oldChords[ chordKey(d) ];
+        if (old) {
+            //old is not undefined, i.e.
+            //there is a matching old chord value
+
+            //check whether source and target have been switched:
+            if (d.source.index != old.source.index ){
+                //swap source and target to match the new data
+                old = {
+                    source: old.target,
+                    target: old.source
+                };
+            }
+
+            tween = d3.interpolate(old, d);
+        }
+        else {
+            //create a zero-width chord object
+            var emptyChord = {
+                source: { startAngle: d.source.startAngle,
+                         endAngle: d.source.startAngle},
+                target: { startAngle: d.target.startAngle,
+                         endAngle: d.target.startAngle}
+            };
+            tween = d3.interpolate( emptyChord, d );
+        }
+
+        return function (t) {
+            //this function calculates the intermediary shapes
+            return path(tween(t));
+        };
+    };
+}
+
+
+/* Activate the buttons and link to data sets */
+d3.select("#HastaButton").on("click", function () {
+    updateChords(transpuesta, 1);
+    //replace this with a file url as appropriate
+
+    //enable other buttons, disable this one
+    disableButton(this);
+});
+
+d3.select("#DesdeButton").on("click", function() {
+    updateChords(m, 0);
+    disableButton(this);
+});
+
+function disableButton(buttonNode) {
+    d3.selectAll("button")
+        .attr("disabled", function(d) {
+            return this === buttonNode? "true": null;
+        });
 }
